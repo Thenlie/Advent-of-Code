c38a1ca4ff767184ea92155e79f50a1611e922db
Thenlie <leithen113@gmail.com>
2022-11-30 22:46:09 -0700

    Solved part 1

--- 1/main.c
+++ 1/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void) {
-	printf("hello world\n");
+    FILE *f = fopen("calories.txt", "r");
+    if (f == NULL) {
+        perror("Unable to read file!");
+    } 
+
+    char buf[64];
+    int elf_count = 0;
+    int max = 0;
+
+    while(fgets(buf, 64, f)) {
+        if (strcmp(buf, "\n") == 0) {
+            if (elf_count > max) {
+                max = elf_count;
+            }
+            printf("%i\n", elf_count);
+            elf_count = 0;
+        }
+        int x = atoi(buf);
+        elf_count += x;
+    }
+    printf("max: %i\n", max);
     return 0;
 }
