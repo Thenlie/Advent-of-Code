1674dbf2c75077511d1a0a8b72fb7c5a565a4eb9
Thenlie <leithen113@gmail.com>
2022-12-02 23:40:01 -0700

    Solved part 2

--- 3/main.c
+++ 3/main.c
@@ -11,36 +11,54 @@ int main (void) {
     }  
 
     char *buf = malloc(128);
+    char *two = malloc(128);
+    char *three = malloc(128);
     int count = 0;
+    int total = 0;
+
     while (fgets(buf, 128, f)) {
-        printf("%s", buf);
-        unsigned long len = strlen(buf);
-        unsigned long mid = len / 2;
-        bool done = false;
-        printf("%lu\n", mid);
-        
-        for (int i = 0; i < (int)mid; i++) {
-            for (int j = (int)mid; j < (int)len; j++) {
-                if (buf[i] == buf[j]) {
-                    printf("%c\n", buf[i]);
-                    if (buf[i] < 91) {
-                        count += buf[i] - 38;
-                        printf("%i\n", buf[i]-38);
+        if (count != 2) {
+            if (count == 0) {
+                strcpy(two, buf);
+                count++;
+            } else {
+                strcpy(three, buf);
+                count++;
+            } 
+        } else {
+            unsigned long len1 = strlen(buf);
+            unsigned long len2 = strlen(two);
+            unsigned long len3 = strlen(three);
+            bool done = false;
+            printf("%s%s%s\n", buf, two, three);
+            
+            for (int i = 0; i < (int)len1; i++) {
+                for (int j = 0; j < (int)len2; j++) {
+                    for (int k = 0; k < (int)len3; k++) {
+                        if (buf[i] == two[j] && buf[i] == three[k] && two[j] == three[k]) {
+                            printf("%c\n", buf[i]);
+                            if (buf[i] < 91) {
+                                total += buf[i] - 38;
+  //                              printf("%i\n", buf[i]-38);
+                            }
+                            else {
+                                total += buf[i] - 96;
+   //                             printf("%i\n", buf[i] - 96);
+                            }
+                            done = true;
+    //                        printf("%i\n", total);
+                            break;
+                        }
                     }
-                    else {
-                        count += buf[i] - 96;
-                        printf("%i\n", buf[i] - 96);
-                    }
-                    done = true;
-                    printf("%i\n", count);
-                    break;
+                    if (done) break;
                 }
+                if (done) break;
             }
-            if (done) break;
+            count = 0;
         }
     }    
 
-    printf("%i\n", count);
+    printf("%i\n", total);
     fclose(f);
     return 0;
 }
