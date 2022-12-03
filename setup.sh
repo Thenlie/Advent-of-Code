d2c40caf717f3a4593ac96a76717544316401590
Thenlie <leithen113@gmail.com>
2022-12-02 21:02:46 -0700

    Created build script. Set up day 3 directory.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir $1
+cd $1
+touch main.c build.sh
+echo $'#!/bin/bash\n\nclang -Wall -Wextra -Wpedantic -Werror -Wshadow -Wformat=2 -Wconversion -Wunused-parameter main.c -o main' > build.sh
+chmod +x ./build.sh
+echo $'#include <stdio.h>\n\nint main (void) {\n    printf("Setup complete!\\n");\n    return 0;\n}\n' > main.c
+./build.sh
+./main
+
