5b6bc0852764d9889e323212de4a0ecb01abfb54
Thenlie <leithen113@gmail.com>
2022-12-04 17:15:22 -0700

    Improved shell script, set up day 5 directory.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Verfiy command line argument
+if [[ ! $1 ]]; then
+    echo "Error: missing argument! Usage './setup.sh <day>'"
+    exit 1
+fi
+
+# Create new directory and files
 mkdir "$1"
 cd "$1"
 touch main.c build.sh
+
+# Write boilerplate to files, run first build
 echo $'#!/bin/bash\n\nclang -Wall -Wextra -Wpedantic -Werror -Wshadow -Wformat=2 -Wconversion -Wunused-parameter main.c -o main' > build.sh
 chmod +x ./build.sh
 echo $'#include <stdio.h>\n\nint main (void) {\n    printf("Setup complete!\\n");\n    return 0;\n}\n' > main.c
