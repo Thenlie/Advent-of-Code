bdd4e99537ee85bcdf11b09ea63bfc69ad0554d9
Thenlie <leithen113@gmail.com>
2022-12-07 23:52:43 -0700

    Solved part 1

--- 7/script.js
+++ 7/script.js
@@ -4,38 +4,28 @@ const data_array = data.split('\n');
 
 let stack = [];
 let total = [];
-let lines = 0;
-let commands = 0;
-let files = 0;
-let lists = 0;
-let cd = 0;
 
 const command_handler = (words) => {
-    commands++;
     if (words[1] === 'cd') {
-        cd++;
         if (words[2] === '..') {
             stack.pop();
         } else {
-            stack.push({name: words[2], size: 0});
-            const index = total.map(x => x.name).indexOf(words[2]);
-            if (index === -1) total.push({name: words[2], size: 0, depth: stack.length});
+            const string = JSON.stringify(stack) + words[2];
+            const index = total.map(x => x.id).indexOf(string);
+            if (index === -1) total.push({name: words[2], size: 0, id: string});
+            stack.push({name: words[2], size: 0, id: string});
         }
-    } else {
-        lists++;
     }
 }
 
 const file_handler = (words) => {
-    files++;
     for (let i = 0; i < stack.length; i++) {
-        const index = total.map(x => x.name).indexOf(stack[i].name);
+        const index = total.map(x => x.id).indexOf(stack[i].id);
         total[index].size += parseInt(words[0]);
     }
 }
 
 data_array.forEach(line => {
-    lines++;
     const words = line.split(' ');
     if (words[0] === '$') command_handler(words);
     else if (words[0] !== 'dir') file_handler(words);
@@ -47,10 +37,3 @@ console.table(answer)
 let sum = 0;
 answer.forEach(ans => sum += ans.size)
 console.log(sum);
-console.log(`
-Lines read: ${lines}
-CD used: ${cd}
-LS used: ${lists}
-Commands: ${commands}
-Files: ${files}
-`)
