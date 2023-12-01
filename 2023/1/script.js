da8eb65761fb2becec112f19ff424ff836c1a071
Leithen Crider <lcrider@cvent.com>
2023-11-30 22:35:41 -0700

    Solved problem 1 part 1

--- 2023/1/script.js
+++ 2023/1/script.js
@@ -1,5 +1,28 @@
 const fs = require('fs');
 const data = fs.readFileSync('input.txt', 'utf8');
-const data_array = data.split('\n');
+const arr = data.split('\n');
 
-console.log(data_array);
+
+let total = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  let val = 0;
+  for (let j = 0; j < arr[i].length; j++) {
+    if (!isNaN(arr[i][j])) {
+      console.log('first')
+      val += arr[i][j];
+      break
+    }
+  }
+  for (let j = arr[i].length; j >= 0; j--) {
+    if (!isNaN(arr[i][j])) {
+      console.log('second')
+      val += arr[i][j];
+      break
+    }
+  }
+  console.log(parseInt(val))
+  total += parseInt(val)
+}
+
+console.log('The answer is: ' + total);
