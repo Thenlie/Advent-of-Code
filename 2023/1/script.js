a3f9effd4db787fc679907ce12912b73d12a3d4f
Leithen Crider <lcrider@cvent.com>
2023-12-01 00:37:44 -0700

    Solved problem 1 part 2

--- 2023/1/script.js
+++ 2023/1/script.js
@@ -2,26 +2,49 @@ const fs = require('fs');
 const data = fs.readFileSync('input.txt', 'utf8');
 const arr = data.split('\n');
 
+const WORDS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 let total = 0;
 
+const containsNumber = (word) => {
+  for (let k = 0; k < WORDS.length; k++) {
+    if (word && word.includes(WORDS[k])) {
+      return k + 1;
+    }
+  }
+  return null;
+}
+
 for (let i = 0; i < arr.length; i++) {
   let val = 0;
+  let first = '';
+  let second = '';
   for (let j = 0; j < arr[i].length; j++) {
     if (!isNaN(arr[i][j])) {
-      console.log('first')
       val += arr[i][j];
       break
+    } else {
+      first = first + arr[i][j];
+      const num = containsNumber(first)
+      if (num) {
+        val += num.toString();
+        break;
+      }
     }
   }
-  for (let j = arr[i].length; j >= 0; j--) {
+  for (let j = arr[i].length - 1; j >= 0; j--) {
     if (!isNaN(arr[i][j])) {
-      console.log('second')
       val += arr[i][j];
       break
+    } else {
+      second = arr[i][j] + second;
+      const num = containsNumber(second)
+      if (num) {
+        val += num.toString();
+        break;
+      }
     }
   }
-  console.log(parseInt(val))
   total += parseInt(val)
 }
 
